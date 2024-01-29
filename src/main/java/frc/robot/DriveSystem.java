71f5f2f456dc2a133cb4528731469417fffe72e8
mnth-titan-robotics <mnth-titan-robotics@gmail.com>
2024-01-29 17:34:43 -0600

    Controller Constants added, DriveSystem.java added

---
+++ src/main/java/frc/robot/DriveSystem.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
+
+import edu.wpi.first.wpilibj.Encoder;
+public class DriveSystem {
+    private Victor FrontLeftMotor;
+    private Victor BackLeftMotor;
+    private Victor FrontRightMotor;
+    private Victor BackRightMotor;
+
+    private Encoder LeftDriveEncoder;
+    private Encoder RightDriveEncoder;
+}
---
+++ src/main/java/frc/robot/RobotConstants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public final class RobotConstants {
+    public static final int FRONT_LEFT_DRIVE_MOTOR_ID = 0;
+    public static final int BACK_LEFT_DRIVE_MOTOR_ID = 1;
+    public static final int FRONT_RIGHT_DRIVE_MOTOR_ID = 2;
+    public static final int BACK_RIGHT_DRIVE_MOTOR_ID = 3;
+    public static final int intake_motor_id = 4;
+    public static final int A_BUTTON = 0;
+    public static final int B_BUTTON = 1;
+    public static final int X_BUTTON = 2;
+    public static final int Y_BUTTON = 3;
+    public static final int LEFT_BUMPER = 4;
+    public static final int RIGHT_BUMPER = 5;
+    
+}
