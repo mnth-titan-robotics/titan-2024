71a2a601a0ddb0b339e5a65c4b6d2092779f39ae
mnth-titan-robotics <mnth-titan-robotics@gmail.com>
2024-03-01 18:22:33 -0600

    auto fix

--- src/main/java/frc/robot/IntakeSystem.java
+++ src/main/java/frc/robot/IntakeSystem.java
@@ -7,20 +7,20 @@ public class IntakeSystem {
     private WPI_TalonSRX LowerIntakeMotor = new WPI_TalonSRX(RobotConstants.LOWER_INTAKE_MOTOR_ID);
     private WPI_TalonSRX UpperIntakeMotor = new WPI_TalonSRX(RobotConstants.UPPER_INTAKE_MOTOR_ID);
     private DigitalInput intakeSwitch = RobotConstants.limitSwitch;
-    private final double speed_modifier = 0.5;
+    private final double lower_speed_modifier = 0.5;
+    private final double upper_speed_modifier = 0.7;
 
     public void update(boolean Button) {
-
         if (!this.intakeSwitch.get()) {
-            this.LowerIntakeMotor.set(ControlMode.PercentOutput, Button ? this.speed_modifier : 0);
-            this.UpperIntakeMotor.set(ControlMode.PercentOutput, Button ? this.speed_modifier : 0);
+            this.LowerIntakeMotor.set(ControlMode.PercentOutput, Button ? this.lower_speed_modifier : 0);
+            this.UpperIntakeMotor.set(ControlMode.PercentOutput, Button ? this.upper_speed_modifier : 0);
         } else {
             this.LowerIntakeMotor.set(ControlMode.PercentOutput,0);
-            this.UpperIntakeMotor.set(ControlMode.PercentOutput, Button ? this.speed_modifier : 0);
+            this.UpperIntakeMotor.set(ControlMode.PercentOutput, Button ? this.lower_speed_modifier : 0);
         }
     }
-    public void eject(boolean Button) {
+    /*public void eject(boolean Button) {
         this.LowerIntakeMotor.set(ControlMode.PercentOutput, Button ? -this.speed_modifier : 0);
         this.UpperIntakeMotor.set(ControlMode.PercentOutput, Button ? -this.speed_modifier : 0);
-    }
+    }*/
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -43,16 +43,18 @@ public class Robot extends TimedRobot {
     //Wait a couple of seconds, then shoot preload
     sleepTimeMilliseconds(2000);
     _ShootSyst.getShoot(true, false);
-    sleepTimeMilliseconds(750);
+    _IntakeSyst.update(true);
+    sleepTimeMilliseconds(3000);
     _ShootSyst.getShoot(false, false);
+    _IntakeSyst.update(false);
 
-    /*//Move back, *collect note*
+    //Move back, *collect note*
     sleepTimeMilliseconds(500);
     //_IntakeSyst.update(true);
-    _DriveSyst.update(-0.3, -0.3);
+    _DriveSyst.update(0.5, 0.5);
     sleepTimeMilliseconds(1000);
     _DriveSyst.update(0,0);
-    //_IntakeSyst.update(false);*/
+    //_IntakeSyst.update(false);
     
     //*Move forward with note*
     /*_DriveSyst.update(0.3, 0.3);
@@ -89,7 +91,7 @@ public class Robot extends TimedRobot {
     _DriveSyst.update(_Ops.LeftDriveStick(), _Ops.RightDriveStick());
     _IntakeSyst.update(_Ops.Right_Bumper());
     _ShootSyst.getShoot(_Ops.B_Button(), _Ops.Left_Bumper());
-    _IntakeSyst.eject(_Ops.Y_Button());
+    //_IntakeSyst.eject(_Ops.Y_Button());
     //_ShootSyst.getRevShoot(_Ops.Left_Bumper());
 
     //talon_motor.set(ControlMode.PercentOutput, this.pilot_joy.getRawAxis(RobotConstants.RIGHT_STICK));
